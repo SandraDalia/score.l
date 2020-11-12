04c48ad64f6bdbcd19b45a436248b9f80c8ab4d5
SandraDalia <64611513+SandraDalia@users.noreply.github.com>
2020-11-12 15:28:27 +0200

    Add initial score.l website files

---
+++ index.js
@@ -0,0 +1,10 @@
+prompt("What is your name?");
+"Sandra"
+prompt("What is his name?");
+"Robert"
+var LoveScore=Math.random()*100;
+undefined
+LoveScore=Math.floor(LoveScore)+1;
+11
+alert("Your love score is" + LoveScore + "%");
+undefined
